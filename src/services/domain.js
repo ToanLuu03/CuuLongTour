819fc633b2f1c1a55cba60abb7c831afd35f6fd1
Luu Truong Toan <luutruongtoanltt@gmail.com>
2025-07-11 22:30:11 +0700

    ai tour

--- src/services/domain.js
+++ src/services/domain.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const domain = axios.create({
   baseURL: "https://cuulongtourbe.onrender.com",
+  // baseURL: "http://localhost:5000"
 });
 
 domain.interceptors.response.use(function (response) {
